2598b18f2f7b36393aee59a739ab1754d0b19876
Rajkumar710394 <44041560+Rajkumar710394@users.noreply.github.com>
2022-10-12 23:27:25 -0400

    Update main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,11 +1,3 @@
-import * as cp from 'child_process'
-import * as path from 'path'
-import {
-  CheckRun,
-  Conclusion,
-  GlobalContext,
-  Status,
-} from '../src/namespaces/Inputs'
 
 const ip = path.join(__dirname, '..', 'lib', 'main.js')
 
